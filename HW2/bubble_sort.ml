41c1b8244afb6f3c35accc2dc5e108d69e35fa9f
barbenhamou <barbenhamou2006@gmail.com>
2024-12-25 17:42:03 +0200

    byebye

--- HW2/bubble_sort.ml
+++ HW2/bubble_sort.ml
@@ -2,5 +2,14 @@ open Ast_sol
 open Nos_sol
 open Semantics_sol
 
+Random.self_init ()
+
+let randimizedArray n max = Array.init n (fun _ -> Random.int max)
+
+let swap 
 
 let run_bubble_sort n =
+  Comp (
+    Ass ("i", 1),
+    While (Gte (Num n, Var ""), )
+  )
